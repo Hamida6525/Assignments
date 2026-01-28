b09a215c68a0468ec8653b0a9dac207bbecc32de
hamidashawky2@gmail.com <hamidashawky2@gmail.com>
2026-01-28 15:50:59 +0200

    resolve

--- assignment_3/lib/Q15.dart
+++ assignment_3/lib/Q15.dart
@@ -1,36 +1,26 @@
 /*Write a Dart program that simulates a simple router using a switch statement on 
   a string path ('/', '/products', '/profile', or other).
-  Handle each case with appropriate output, including maps and null safety where needed.*/
+  Handle each case with appropriate output,
+  including maps and null safety where needed.*/
 
 void main() {
-  String path = '/products';
-
+  String path = '/profile';
+  Map<String, String> screens = {
+    '/': 'Home Page',
+    '/products': 'Products Page',
+    '/profile': 'Profile Page', 
+  };
   switch (path) {
     case '/':
-      print('Home Page');
+      print(screens['/']);
       break;
     case '/products':
-      Map<String, double> products = {
-        'Laptop': 1000.90,
-        'Smartphone': 800.99,
-        'Tablet': 500.99,
-      };
-      print('Products Page:');
-      products.forEach((product, price) {
-        print('$product: \$$price');
-      });
+      print(screens['/products']);
       break;
     case '/profile':
-      Map<String, String?> userProfile = {
-        'name': 'John Doe',
-        'phone': null, // Simulating a null value
-      };
-      String phone = userProfile['phone'] ?? 'Phone number not provided';
-      print('Profile Page:');
-      print('Name: ${userProfile['name']}');
-      print('Phone: $phone');
+      print(screens['/profile']);
       break;
     default:
-      print('other');
+      print('Not Found Screen');
   }
 }
