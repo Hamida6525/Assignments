861b6878fa339756f15d4affd164fb429c224f71
hamidashawky2@gmail.com <hamidashawky2@gmail.com>
2026-02-17 11:13:13 +0200

    another solution

--- assignment_9/LeetCode.dart
+++ assignment_9/LeetCode.dart
@@ -2,15 +2,24 @@
   You must implement a solution with a linear runtime complexity and use only constant extra space.*/
  
 
+ 
 int singleNumber(List<int> nums) {
- Map<int, int> countMap = {};
-    for (int num in nums) {
-      countMap[num] = (countMap[num] ?? 0) + 1;
-    }
-    for (var entry in countMap.entries) {
-      if (entry.value == 1) {
-        return entry.key;
-      }
-    }
-    return -1; 
+  int result = 0;
+  for (int num in nums) {
+    result ^= num;
   }
+  return result;
+} 
+
+// int singleNumber(List<int> nums) {
+//  Map<int, int> countMap = {};
+//     for (int num in nums) {
+//       countMap[num] = (countMap[num] ?? 0) + 1;
+//     }
+//     for (var entry in countMap.entries) {
+//       if (entry.value == 1) {
+//         return entry.key;
+//       }
+//     }
+//     return -1; 
+//   }
